c91d66002c89e8566d265379e16e8ebb30dfc2cf
Victor <pinkpolarbear@yandex.ru>
2023-07-01 17:01:18 +0300

    user controls style
    from tailwind to scss modules

--- src/components/UserControls/UserControls.tsx
+++ src/components/UserControls/UserControls.tsx
@@ -1,22 +1,15 @@
-import UserControlButton from '../../ui/buttons/UserControlButton'
+import UserControlButton from "../../ui/buttons/UserControlButton";
+
+import styles from "./UserControls.module.scss";
 
 const UserControls = () => {
   return (
-    <div className='flex flex-col gap-10 w-16 items-center shadow-buttons px-4 py-11 rounded-full'>
-      <UserControlButton
-        route='friends'
-        icon='friends.svg'
-      />
-      <UserControlButton
-        route='discovery'
-        icon='compass.svg'
-      />
-      <UserControlButton
-        route='profile'
-        icon='legasov.png'
-      />
+    <div className={styles.container}>
+      <UserControlButton route="friends" icon="friends.svg" />
+      <UserControlButton route="discovery" icon="compass.svg" />
+      <UserControlButton route="profile" icon="legasov.png" />
     </div>
-  )
-}
+  );
+};
 
-export default UserControls
+export default UserControls;
