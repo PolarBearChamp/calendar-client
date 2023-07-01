6bd0846d2dc1d259577414d539ad4476c39796ef
Victor <pinkpolarbear@yandex.ru>
2023-07-01 16:33:21 +0300

    day styles
    from tailwind to scss modules

--- src/components/Calendar/Day/Day.tsx
+++ src/components/Calendar/Day/Day.tsx
@@ -1,36 +1,35 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from "next/image";
+import React from "react";
+import classNames from "classnames";
+import styles from "./Day.module.scss";
 
 interface Props {
-  day: number
-  scene?: string
+  day: number;
+  scene?: string;
 }
 const Day = ({ day, scene }: Props) => {
-
   if (!!scene) {
     return (
-      <div className='shadow-custom w-36 h-36 rounded-xl relative overflow-hidden'>
-        <span className='absolute z-10 mt-3 ml-5 text-2xl font-bold text-white'>
+      <div className={classNames(styles.container)}>
+        <span className={classNames(styles.number, styles.withBack)}>
           {day}
         </span>
         <Image
           src={`${scene}`}
-          alt=''
+          alt=""
           width={100}
           height={100}
-          className='object-cover object-center h-full w-full brightness-50'
+          className={styles.back}
         />
       </div>
-    )
+    );
   } else {
     return (
-      <div className='shadow-custom w-36 h-36 rounded-xl relative  overflow-hidden'>
-        <span className='absolute mt-3 ml-5 text-2xl font-bold'>
-          {day}
-        </span>
+      <div className={classNames(styles.container)}>
+        <span className={styles.number}>{day}</span>
       </div>
-    )
+    );
   }
-}
+};
 
-export default Day
+export default Day;
