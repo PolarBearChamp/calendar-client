2ff0c11c13c56b6f6fa63ed236c63988cf458f2e
Victor <pinkpolarbear@yandex.ru>
2023-07-22 22:31:34 +0300

    FIX Button component

---
+++ src/ui/Buttons/Button.tsx
@@ -0,0 +1,21 @@
+import { ButtonHTMLAttributes, FC } from "react"
+import { clsx } from "clsx"
+import cls from "./Button.module.scss"
+
+export const enum ButtonTheme {
+    PRIMARY = "primary",
+}
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+    className?: string
+    theme: ButtonTheme
+}
+
+export const Button: FC<ButtonProps> = (props) => {
+    const { className, children, theme, ...otherProps } = props
+    return (
+        <button className={clsx(cls.Button, className, cls[theme])} {...otherProps}>
+            {children}
+        </button>
+    )
+}
