29c09126032cef24f7894c0a9c2c879f05e39888
Victor <pinkpolarbear@yandex.ru>
2023-07-31 16:21:50 +0300

    add some styles for dark theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
-            <body className={clsx("app")}>
+            <body className={clsx("app light")}>
                 {/* <Logo /> */}
                 {children}
             </body>
--- src/components/Login/Forms/LoginForm.tsx
+++ src/components/Login/Forms/LoginForm.tsx
@@ -7,58 +7,70 @@ import PrimaryInput from "@/src/ui/inputs/PrimaryInput"
 import styles from "./LoginForm.module.scss"
 import { ILoginData } from "@/src/types"
 import { CustomLink, LinkTheme } from "@/src/ui/Links/CustomLink"
+import { useRouter } from "next/navigation"
 
 const LoginForm: React.FC = () => {
+    const router = useRouter()
+
     const {
         register,
         handleSubmit,
         formState: { errors },
     } = useForm()
-    const onSubmit = handleSubmit((data) => console.log(data))
+    const onSubmit = handleSubmit((data) => {
+        console.log(data)
+        router.push("/home")
+    })
     console.log(errors)
 
     return (
         <form onSubmit={onSubmit} className={styles.form}>
             <h1 className={styles.header}>Login</h1>
-            <label htmlFor="email">
-                Email
-                <PrimaryInput
-                    id="email"
-                    name="email"
-                    type="email"
-                    placeholder="username@mail.com"
-                    register={register}
-                    options={{ required: true }}
-                />
-            </label>
-            {errors.email && errors.email.type === "required" && <span>Поле обязательно</span>}
-            <label htmlFor="password">
-                Password
-                <PrimaryInput
-                    id="password"
-                    name="password"
-                    type="password"
-                    placeholder="password"
-                    register={register}
-                    options={{ required: true, minLength: 6 }}
-                />
-            </label>
-            {errors.password && errors.password.type === "required" && (
-                <span>Поле обязательно</span>
-            )}
-            {errors.password && errors.password.type === "minLength" && (
-                <span>Минимальная длина 6 символов</span>
-            )}
-            <CustomLink href="/forget" theme={LinkTheme.DEFAULT}>
-                bro, did you really forget the password?
-            </CustomLink>
-            <Button theme={ButtonTheme.PRIMARY}>Sign in</Button>
-            <span>
-                bro, you really don&apos;t have an account?
-                <CustomLink href="/" theme={LinkTheme.DEFAULT}>
-                    &nbsp;Sign up for free
+            <div className={styles.fields}>
+                <label htmlFor="email">
+                    Email
+                    <PrimaryInput
+                        id="email"
+                        name="email"
+                        type="email"
+                        placeholder="username@mail.com"
+                        register={register}
+                        options={{ required: true }}
+                    />
+                </label>
+                {errors.email && errors.email.type === "required" && (
+                    <div className={styles.warning}>Поле обязательно</div>
+                )}
+                <label htmlFor="password">
+                    Password
+                    <PrimaryInput
+                        id="password"
+                        name="password"
+                        type="password"
+                        placeholder="password"
+                        register={register}
+                        options={{ required: true, minLength: 6 }}
+                    />
+                </label>
+                {errors.password && errors.password.type === "required" && (
+                    <div className={styles.warning}>Поле обязательно</div>
+                )}
+                {errors.password && errors.password.type === "minLength" && (
+                    <div className={styles.warning}>Минимальная длина 6 символов</div>
+                )}
+                <CustomLink href="/forget" theme={LinkTheme.DEFAULT}>
+                    bro, did you really forget the password?
                 </CustomLink>
-            </span>
+            </div>
+            <div className={styles.submit}>
+                <Button theme={ButtonTheme.PRIMARY}>Sign in</Button>
+                <span>
+                    bro, you really don&apos;t have an account?
+                    <CustomLink href="/" theme={LinkTheme.DEFAULT}>
+                        &nbsp;Sign up for free
+                    </CustomLink>
+                </span>
+            </div>
         </form>
     )
 }
