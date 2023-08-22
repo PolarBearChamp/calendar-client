322ebcb11d0cc4bac86d2389bbe92756d9b394ee
Victor <pinkpolarbear@yandex.ru>
2023-08-22 19:42:04 +0300

    replace profile and friends

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import cls from './LoginModule.module.scss'
+import cls from './LoginPage.module.scss'
 import { FC } from 'react'
 import LoginForm from '@/FeatureLayer/Login/ui/LoginForm/LoginForm'
 import Slides from '@/SharedLayer/ui/Slides/Slides'
