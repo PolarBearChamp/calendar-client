f251fd5db07b8f7c6971338548d15fbde064f066
Victor <pinkpolarbear@yandex.ru>
2023-08-22 19:36:28 +0300

    replace slides

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 import cls from './LoginModule.module.scss'
 import { FC } from 'react'
 import LoginForm from '@/FeatureLayer/Login/ui/LoginForm/LoginForm'
-import Slides from '@/components/Login/Slides/Slides'
+import Slides from '@/SharedLayer/ui/Slides/Slides'
 
 const LoginPage: FC = () => {
   return (
