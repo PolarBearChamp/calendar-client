bcad2f5d67ace40bb60d60b7c9be17454a0b9a53
Victor <pinkpolarbear@yandex.ru>
2023-08-22 16:23:50 +0300

    replace styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,18 @@
-"use client"
+'use client'
 
-import "@/styles/index.scss"
-import { clsx } from "clsx"
-import { ReactNode } from "react"
-import { ThemeProvider } from "@/AppLayer/providers/ThemeProvider"
+import '@/AppLayer/style/index.scss'
+import { clsx } from 'clsx'
+import { ReactNode } from 'react'
+import { ThemeProvider } from '@/AppLayer/providers/ThemeProvider'
 
 export default function RootLayout({ children }: { children: ReactNode }) {
-    const theme = localStorage.getItem("theme") || "light"
+  const theme = localStorage.getItem('theme') || 'light'
 
-    return (
-        <ThemeProvider>
-            <html lang="en">
-                <body className={clsx(`app ${theme}`)}>{children}</body>
-            </html>
-        </ThemeProvider>
-    )
+  return (
+    <ThemeProvider>
+      <html lang="en">
+        <body className={clsx(`app ${theme}`)}>{children}</body>
+      </html>
+    </ThemeProvider>
+  )
 }
