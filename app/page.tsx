bc2f8fe47142bce312d06b127b8352333c896a5c
Victor <pinkpolarbear@yandex.ru>
2023-08-22 19:06:00 +0300

    replace login module

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
-"use client"
+'use client'
 
-import LoginModule from "@/modules/LoginModule/LoginModule"
+import cls from './LoginModule.module.scss'
+import { FC } from 'react'
+import LoginForm from '@/FeatureLayer/Login/ui/LoginForm/LoginForm'
+import Slides from '@/components/Login/Slides/Slides'
 
-const page = () => {
-    return <LoginModule />
+const LoginPage: FC = () => {
+  return (
+    <div className={cls.container}>
+      <LoginForm />
+      <Slides />
+    </div>
+  )
 }
 
-export default page
+export default LoginPage
