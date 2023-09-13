2367b5b513dd700159c4faa2c2e3bd0bbe99eb15
PolarBearChamp <pinkpolarbear@yandex.ru>
2023-09-13 15:01:22 +0300

    style fixes

--- src/SharedLayer/ui/Slides/SlideItem/SlideItem.tsx
+++ src/SharedLayer/ui/Slides/SlideItem/SlideItem.tsx
@@ -1,18 +1,24 @@
-import Image from "next/image"
-import { clsx } from "clsx"
+import Image from 'next/image'
+import { clsx } from 'clsx'
 
-import { ISlideItemData } from "@/types"
-import cls from "./SlideItem.module.scss"
+import cls from './SlideItem.module.scss'
+import { ISlideItemData } from '@/SharedLayer/model'
 
 const SlideItem: React.FC<ISlideItemData> = ({ text, type, image }) => (
-    <div
-        className={clsx(cls.container, {
-            [cls.text]: type === "text",
-        })}
-    >
-        {text}
-        <Image src={image} alt="slideItem" width={200} height={200} className={cls.image} />
-    </div>
+  <div
+    className={clsx(cls.container, {
+      [cls.text]: type === 'text',
+    })}
+  >
+    {text}
+    <Image
+      src={image}
+      alt="slideItem"
+      width={200}
+      height={200}
+      className={cls.image}
+    />
+  </div>
 )
 
 export default SlideItem
