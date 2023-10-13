4db34ba0dd12df257b8cd026e0763ba01c9ce5d1
Victor <pinkpolarbear@yandex.ru>
2023-10-14 01:55:57 +0300

    fix server

--- server/server.js
+++ server/server.js
@@ -1,20 +1,15 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const fs = require('fs')
+const cors = require('cors')
 
 const app = express()
 const PORT = process.env.PORT || 8000
-
-// Middleware для обработки JSON-тела запросов
+app.use(cors())
 app.use(bodyParser.json())
 
 // JSON-файл, который будет использоваться как база данных
 const databaseFile = './server/db.json'
-
-// Создаем пустой JSON-файл, если его нет
-if (!fs.existsSync(databaseFile)) {
-  fs.writeFileSync(databaseFile, JSON.stringify({ users: [], tasks: [] }))
-}
 
 // Функция для чтения базы данных из файла
 function readDatabase() {
@@ -28,19 +23,26 @@ function writeDatabase(data) {
 }
 
 // Регистрация нового пользователя
-app.post('/register', (req, res) => {
-  const { username, password } = req.body
+app.post('/signup', (req, res) => {
+  const { username, password, email } = req.body
   const database = readDatabase()
 
-  // Проверяем, что пользователь с таким именем не существует
+  // Проверяем, что пользователя с таким именем не существует
   if (database.users.some((user) => user.username === username)) {
     return res
       .status(400)
-      .json({ error: 'Пользователь с таким именем уже существует' })
+      .json({ error: 'Пользователь с таким Username уже существует' })
   }
 
   // Создаем нового пользователя и добавляем его в базу данных
-  const newUser = { username, password }
+  const newUser = {
+    id: (database.users.length + 1).toString(),
+    email,
+    username,
+    password,
+    birthday: '',
+  }
+
   database.users.push(newUser)
   writeDatabase(database)
 
@@ -59,40 +61,97 @@ app.post('/login', (req, res) => {
     return res.status(401).json({ error: 'Неверные учетные данные' })
   }
 
-  res.json({ message: 'Вход выполнен успешно' })
+  res.json({ message: 'Вход выполнен успешно', token: user.id })
 })
 
-// Добавление новой задачи
-app.post('/add_task', (req, res) => {
-  const { task } = req.body
+// Добавление новой песни
+app.post('/posts/add', (req, res) => {
+  const {
+    songName,
+    artistName,
+    description,
+    songLink,
+    imageLink,
+    tags,
+    userId,
+  } = req.body
+
   const database = readDatabase()
 
-  // Добавляем новую задачу в базу данных
-  const newTask = { task }
-  database.tasks.push(newTask)
+  // Добавляем новую песню в базу данных
+  const newPost = {
+    id: (database.posts.length + 1).toString(),
+    songName,
+    artistName,
+    description,
+    songLink,
+    imageLink,
+    tags,
+    userId,
+  }
+  database.posts.push(newPost)
   writeDatabase(database)
 
-  res.json({ message: 'Задача успешно добавлена' })
+  res.json({
+    success: true,
+    body: {
+      postId: newPost.id,
+    },
+  })
 })
 
-app.get('/tasks', (req, res) => {
+// Получение всех Posts
+app.get('/posts', (req, res) => {
   const database = readDatabase()
-  res.json(database.tasks)
+  const posts = database.posts
+
+  res.json({
+    success: true,
+    body: { posts },
+  })
 })
 
-// Получение задачи по идентификатору (ID)
-app.get('/tasks/:taskId', (req, res) => {
-  const taskId = parseInt(req.params.taskId)
+// Получение Post по идентификатору (ID)
+app.get('/posts/:postId', (req, res) => {
+  const postId = req.params.postId
   const database = readDatabase()
 
   // Найдите задачу с заданным ID
-  const task = database.tasks.find((task) => task.id === taskId)
+  const post = database.posts.find((post) => post.id === postId)
 
-  if (!task) {
-    return res.status(404).json({ error: 'Задача не найдена' })
+  if (!post) {
+    return res.status(404).json({ error: 'Post не найден' })
   }
 
-  res.json(task)
+  res.json({
+    success: true,
+    body: {
+      post,
+    },
+  })
+})
+
+app.get('/posts/filter', (req, res) => {
+  const { date, userId } = req.query
+
+  if (!date || !userId) {
+    return res
+      .status(400)
+      .json({ error: 'Параметры date и userId обязательны' })
+  }
+  const database = readDatabase()
+  const allPosts = database.posts
+
+  const filteredPosts = allPosts.filter(
+    (post) => post.date.startsWith(date) && post.userId === parseInt(userId),
+  )
+
+  res.json({
+    success: true,
+    body: {
+      filteredPosts,
+    },
+  })
 })
 
 app.listen(PORT, () => {
